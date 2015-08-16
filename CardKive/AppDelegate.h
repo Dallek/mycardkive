fee4aec900140eba82fffd07b888422ecd402109
Mini <mini@Minis-Mac-mini.local>
2015-08-16 15:41:16 -0700

    Initial Commit

---
+++ CardKive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardKive
+//
+//  Created by Mini on 8/16/15.
+//  Copyright (c) 2015 99Ideas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardKive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardKive
+//
+//  Created by Mini on 8/16/15.
+//  Copyright (c) 2015 99Ideas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
